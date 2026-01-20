0e7b251dcba2a9877120e2ad8bc6277a29c4f624
Nikhitakunthu52 <nikkikunthu@gmail.com>
2026-01-19 19:59:07 -0500

    Working LED Script

--- Deliverable03/LEDScript.py
+++ Deliverable03/LEDScript.py
@@ -1,5 +1,5 @@
 import pigpio
-import time
+#import time
 pi1 = pigpio.pi()
 
 ledPin = 18
@@ -11,29 +11,29 @@ pi1.set_pull_up_down(switchPin, pigpio.PUD_UP)
 
 pi1.set_glitch_filter(switchPin, 50000)
 
-print("LED Off")
+#print("LED Off")
 
-print("LED Pin:", pi1.read(ledPin))
-print("Switch Pin:", pi1.read(switchPin))
+#print("LED Pin:", pi1.read(ledPin))
+#print("Switch Pin:", pi1.read(switchPin))
 
-pi1.write(ledPin, 0)
-print("LED Pin:", pi1.read(ledPin))
-print("Switch Pin:", pi1.read(switchPin))
+#pi1.write(ledPin, 0)
+#print("LED Pin:", pi1.read(ledPin))
+#print("Switch Pin:", pi1.read(switchPin))
 
-time.sleep(15)
+#time.sleep(15)
 
-pi1.write(ledPin, 1)
-print("LED Pin:", pi1.read(ledPin))
-print("Switch Pin:", pi1.read(switchPin))
+#pi1.write(ledPin, 1)
+#print("LED Pin:", pi1.read(ledPin))
+#print("Switch Pin:", pi1.read(switchPin))
 
-print("End Script")
+#print("End Script")
 
-#while True:
-#    pi1.wait_for_edge(switchPin)
-#    if pi1.read(ledPin) == 0:
-#        pi1.write(ledPin, 1)
-#        print("LED On")
-#    elif pi1.read(ledPin) == 1:
-#        pi1.write(ledPin, 0)
-#        print("LED Off")
+while True:
+    pi1.wait_for_edge(switchPin)
+    if pi1.read(ledPin) == 1:
+        pi1.write(ledPin, 0)
+        print("LED On")
+    elif pi1.read(ledPin) == 0:
+        pi1.write(ledPin, 1)
+        print("LED Off")
 
