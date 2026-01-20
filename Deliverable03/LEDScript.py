ed3858917c6f9d27e547ecdc5edb639be2d53d1d
Nikhitakunthu52 <nikkikunthu@gmail.com>
2026-01-19 19:14:39 -0500

    Modified LEDScript

--- Deliverable03/LEDScript.py
+++ Deliverable03/LEDScript.py
@@ -7,7 +7,7 @@ switchPin = 24
 
 pi1.set_mode(ledPin, pigpio.OUTPUT)
 pi1.set_mode(switchPin, pigpio.INPUT)
-pi1.set_pull_up_down(switchPin, pigpio.PUD_DOWN)
+pi1.set_pull_up_down(switchPin, pigpio.PUD_UP)
 
 pi1.set_glitch_filter(switchPin, 50000)
 
@@ -16,13 +16,13 @@ print("LED Off")
 print("LED Pin:", pi1.read(ledPin))
 print("Switch Pin:", pi1.read(switchPin))
 
-pi1.write(ledPin, 1)
+pi1.write(ledPin, 0)
 print("LED Pin:", pi1.read(ledPin))
 print("Switch Pin:", pi1.read(switchPin))
 
-time.sleep(5)
+time.sleep(15)
 
-pi1.write(ledPin, 0)
+pi1.write(ledPin, 1)
 print("LED Pin:", pi1.read(ledPin))
 print("Switch Pin:", pi1.read(switchPin))
 
