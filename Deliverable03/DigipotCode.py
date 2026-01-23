6a0403f41f07af9f46faf26878b521c7902a5400
thomasprybylski <top91741@uga.edu>
2026-01-23 14:58:11 -0500

    Update DigipotCode.py

--- Deliverable03/DigipotCode.py
+++ Deliverable03/DigipotCode.py
@@ -30,7 +30,7 @@ try:
         try:
             val = int(user_input)
             if 0 <= val <= 128:
-                write_pot(val)
+                set_pot_step(val)
             else:
                 print("Error: Please enter a number between 0 and 128.")
         except ValueError:
