f5d52e90f99406e4b9b130a3ecd7a417ba38f051
Nikhita Kunthu <nikkikunthu@gmail.com>
2026-01-26 20:12:27 -0500

    Update LEDScript.py
    added comments to the LEDScript.py file

--- Deliverable03/LEDScript.py
+++ Deliverable03/LEDScript.py
@@ -1,17 +1,23 @@
 import pigpio
 # Importing Library
 
+# Initializing the connection to the pigpio daemon
 pi1 = pigpio.pi()
 
-ledPin = 18
-switchPin = 24
+# Defining Pin Numbers
+ledPin = 18 #LED
+switchPin = 24 #Switch/encoder button
 
-pi1.set_mode(ledPin, pigpio.OUTPUT)
-pi1.set_mode(switchPin, pigpio.INPUT)
-pi1.set_pull_up_down(switchPin, pigpio.PUD_UP)
+pi1.set_mode(ledPin, pigpio.OUTPUT) # Set LED Pin as output
+pi1.set_mode(switchPin, pigpio.INPUT) #Set switch pin as input
 
+# This keeps the input at 3.3V (HIGH) until the switch is pressed
+pi1.set_pull_up_down(switchPin, pigpio.PUD_UP) 
+
+# This ignores signal noise/bouncing for 50,000 microseconds to ensure clean triggering
 pi1.set_glitch_filter(switchPin, 50000)
 
+# Use hardware-timed edge detection to wait for a switch press, then toggle the LED's current state.
 while True:
     pi1.wait_for_edge(switchPin)
     if pi1.read(ledPin) == 1:
