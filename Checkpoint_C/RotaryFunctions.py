02335678e048fd46eecc65777ac612906496b4e3
SenWasHere <152115255+SenWasHere@users.noreply.github.com>
2026-02-08 15:29:58 -0500

    Update RotaryFunctions.py

--- Checkpoint_C/RotaryFunctions.py
+++ Checkpoint_C/RotaryFunctions.py
@@ -85,14 +85,14 @@ class Rotary:
         while True:
 
             #if button has been pressed
-            if self.pi1.wait_for_edge(self.switchPin):
+            if self.pi1.wait_for_edge(self.switchPin, 2):
                 self.clicked = True
                 startTime = time.perf_counter()
                 print("Press")
                 print("Start Time Button:", startTime)
 
                 #if button is no longer pressed
-                if self.pi1.wait_for_edge(self.switchPin, pigpio.FALLING_EDGE):
+                if self.pi1.wait_for_edge(self.switchPin, pigpio.FALLING_EDGE, 2):
                     endTime = time.perf_counter()
                     print("Stop Press")
                     print("End Time Button:", endTime)
