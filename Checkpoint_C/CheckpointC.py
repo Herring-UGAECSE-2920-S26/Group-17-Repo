7f527ce1e1f95e64a33fba5c35738b4a4b84a44b
SenWasHere <152115255+SenWasHere@users.noreply.github.com>
2026-02-08 14:49:37 -0500

    Make it so prints only happen in CheckpointC.py when something has updated

--- Checkpoint_C/CheckpointC.py
+++ Checkpoint_C/CheckpointC.py
@@ -32,68 +32,93 @@ digi1R = 100
 digi2R = 100
 minR = 100
 maxR = 10000
+menu1First = True
+menu2First = True
+digi1First = True
+digi2First = True
 
 while True:
 
     #main menu part 1
     while state == "menu1":
-        print("=> DigiPot1    Digipot2")
-        #insert proper LCD updating code here
+        if menu1First == True:
+            print("=> DigiPot1    Digipot2")
+            #insert proper LCD updating code here
         if rotary.rotating == True:
             state = "menu2"
+            menu2First = True
         elif rotary.clicked == True:
             state = "digi1"
+            digi1First = True
+        menu1First = False
 
         #main menu part 2
         while state == "menu2":
-            #insert proper LCD updating code here
-            print("   DigiPot1 => Digipot2")
+            if menu2First == True:
+                #insert proper LCD updating code here
+                print("   DigiPot1 => Digipot2")
             if rotary.rotating == True:
                 state = "menu1"
+                menu1First = True
             elif rotary.clicked == True:
                 state = "digi2"
+                digi2First = True
+            menu2First = False
 
         #DigiPot1 handling
         while state == "digi1":
-            #insert proper LCD updating code here
-            print(digi1R)
+            if digi1First == True:
+                #insert proper LCD updating code here
+                print(digi1R)
             if rotary.rotating == True:
                 if rotary.clockwise == True and digi1R != maxR:
                     if rotary.fast == True:
                         digi1R = digi1R + 100
+                        digi1First = True
                     else:
                         digi1R = digi1R + 10
+                        digi1First = True
                 if rotary.clockwise == False and digi1R != minR:
                     if rotary.fast == True:
                         digi1R = digi1R - 100
+                        digi1First = True
                     else:
                         digi1R = digi1R - 10
+                        digi1First = True
             elif rotary.clicked == True:
                 if rotary.longClick == True:
                     state = "menu1"
+                    menu1First = True
                 else:
                     print("Updated DigiPot1")
                     #insert proper digipot updating code here
+            digi1First = False
 
         #DigiPot2 handling
         while state == "digi2":
-            #insert proper LCD updating code here
-            print(digi2R)
+            if digi2First == True:
+                #insert proper LCD updating code here
+                print(digi2R)
             if rotary.rotating == True:
                 if rotary.clockwise == True and digi2R != maxR:
                     if rotary.fast == True:
                         digi2R = digi2R + 100
+                        digi2First = True
                     else:
                         digi2R = digi2R + 10
                 if rotary.clockwise == False and digi1R != minR:
                     if rotary.fast == True:
                         digi2R = digi2R - 100
+                        digi2First = True
                     else:
                         digi2R = digi2R - 10
+                        digi2First = True
             elif rotary.clicked == True:
                 if rotary.longClick == True:
                     state = "menu1"
+                    menu1First = True
                 else:
                     print("Updated DigiPot2")
                     #insert proper digipot updating code here
+            digi2First = False
     
