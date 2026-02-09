8bb9c59fc0c8e3c265fbd9e84b4469d7461bfea3
thomasprybylski <top91741@uga.edu>
2026-02-09 14:27:48 -0500

    Add min_difference to rotary threading branch
    Implement min_difference function to find the closest resistance step.

---
+++ Checkpoint_C/Min_difference.py
@@ -0,0 +1,16 @@
+def min_difference(digi1R):
+    select_resistance = digi1R
+    min_difference = float(100000) #sets the value used to compare to differnce very high so that any mesured
+
+    for i in range(0, 128): # 1 - 128 steps of digipot
+        resistance = 0.0732*i + 0.124 # characteristic eqn of our dual digipot
+        difference = abs(selected_resistance - resistance) # goes through difference between every resistance at each step and the resistance choosen by the user #
+
+    if difference < min_difference: # goes through range and sets min_diff to smallest difference and sets i at that step
+        min_difference = difference
+        step = i
+
+print(f"set to step: {step}")
+if __name__ == "__main__":
+    selected_resistance = float(input('resistance in kOhms'))
+    
