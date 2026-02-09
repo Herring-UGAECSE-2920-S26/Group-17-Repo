2e250fe7d04c1a6a82f7987c15ee2835f5dccb3f
Nikhita Kunthu <nikkikunthu@gmail.com>
2026-02-09 11:06:05 -0500

    Update CheckpointC.py

--- Checkpoint_C/CheckpointC.py
+++ Checkpoint_C/CheckpointC.py
@@ -2,38 +2,86 @@ import smbus
 from time import sleep
 
 I2CBUS = 1
-ADDRESS = 0x27
 
-# LCD Constants (Omitted for brevity, use the ones from your original code)
-# ... LCD_CLEARDISPLAY, LCD_RETURNHOME, etc ...
+# commands
+LCD_CLEARDISPLAY = 0x01
+LCD_RETURNHOME = 0x02
+LCD_ENTRYMODESET = 0x04
+LCD_DISPLAYCONTROL = 0x08
+LCD_CURSORSHIFT = 0x10
+LCD_FUNCTIONSET = 0x20
+LCD_SETCGRAMADDR = 0x40
+LCD_SETDDRAMADDR = 0x80
+
+# flags for display entry mode
+LCD_ENTRYLEFT = 0x02
+
+# flags for display on/off control
+LCD_DISPLAYON = 0x04
+
+# flags for function set
+LCD_8BITMODE = 0x10
+LCD_4BITMODE = 0x00
+LCD_2LINE = 0x08
+LCD_5x8DOTS = 0x00
+
+# flags for backlight control
+LCD_BACKLIGHT = 0x08
+LCD_NOBACKLIGHT = 0x00
+
+En = 0b00000100 # Enable bit
+Rs = 0b00000001 # Register select bit
 
 class i2c_device:
-    def __init__(self, addr, port=I2CBUS):
-        self.addr = addr
-        self.bus = smbus.SMBus(port)
+   def __init__(self, addr, port=I2CBUS):
+      self.addr = addr
+      self.bus = smbus.SMBus(port)
 
-    def write_cmd(self, cmd):
-        self.bus.write_byte(self.addr, cmd)
-        sleep(0.0001)
+   def write_cmd(self, cmd):
+      self.bus.write_byte(self.addr, cmd)
+      sleep(0.0001)
 
 class LCD:
-    def __init__(self, addr=0x27):
-        self.lcd_device = i2c_device(addr)
-        # Initialization sequence (From your original code)
-        self.lcd_write(0x03)
-        self.lcd_write(0x03)
-        self.lcd_write(0x03)
-        self.lcd_write(0x02)
-        # ... rest of init ...
+   def __init__(self, addr=0x27):
+      self.lcd_device = i2c_device(addr)
+      self.lcd_write(0x03)
+      self.lcd_write(0x03)
+      self.lcd_write(0x03)
+      self.lcd_write(0x02)
+      self.lcd_write(LCD_FUNCTIONSET | LCD_2LINE | LCD_5x8DOTS | LCD_4BITMODE)
+      self.lcd_write(LCD_DISPLAYCONTROL | LCD_DISPLAYON)
+      self.lcd_write(LCD_CLEARDISPLAY)
+      sleep(0.002) 
+      self.lcd_write(LCD_ENTRYMODESET | LCD_ENTRYLEFT)
 
-    def lcd_write(self, cmd, mode=0):
-        # Implementation from your original code
-        pass
+   def lcd_strobe(self, data):
+      self.lcd_device.write_cmd(data | En | LCD_BACKLIGHT)
+      sleep(.0005)
+      self.lcd_device.write_cmd(((data & ~En) | LCD_BACKLIGHT))
+      sleep(.0001)
 
-    def lcd_display_string(self, string, line=1, pos=0):
-        # Implementation from your original code
-        pass
+   def lcd_write_four_bits(self, data):
+      self.lcd_device.write_cmd(data | LCD_BACKLIGHT)
+      self.lcd_strobe(data)
 
-    def lcd_clear(self):
-        # Implementation from your original code
-        pass
+   def lcd_write(self, cmd, mode=0):
+      self.lcd_write_four_bits(mode | (cmd & 0xF0))
+      self.lcd_write_four_bits(mode | ((cmd << 4) & 0xF0))
+  
+   def lcd_display_string(self, string, line=1, pos=0):
+    if line == 1:
+      pos_new = pos
+    elif line == 2:
+      pos_new = 0x40 + pos
+    elif line == 3:
+      pos_new = 0x14 + pos
+    elif line == 4:
+      pos_new = 0x54 + pos
+
+    self.lcd_write(0x80 + pos_new)
+    for char in string:
+      self.lcd_write(ord(char), Rs)
+
+   def lcd_clear(self):
+      self.lcd_write(LCD_CLEARDISPLAY)
+      sleep(0.002)
