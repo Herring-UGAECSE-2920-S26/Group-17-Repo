4b56076e84f7601319a124d832e1cc577de886dd
Nikhita Kunthu <nikkikunthu@gmail.com>
2026-02-09 11:05:05 -0500

    Update RotaryFunctions.py

--- Checkpoint_C/RotaryFunctions.py
+++ Checkpoint_C/RotaryFunctions.py
@@ -1,79 +1,42 @@
 import pigpio
-import asyncio
 import time
+import asyncio
 
 class Rotary:
+    minR = 110
+    maxR = 9422
 
-    #min and max resistor values
-    minR = 100
-    maxR = 10000
-
-    #initialize Rotary object
     def __init__(self, rotaryA, rotaryB, switchPin, pi1):
         self.rotaryA = rotaryA
         self.rotaryB = rotaryB
         self.switchPin = switchPin
         self.pi1 = pi1
 
-        #set up rotary encoder
-        self.pi1.set_mode(self.rotaryA, pigpio.INPUT)
-        self.pi1.set_pull_up_down(self.rotaryA, pigpio.PUD_UP)
-        
-        self.pi1.set_mode(self.rotaryB, pigpio.INPUT)
-        self.pi1.set_pull_up_down(self.rotaryB, pigpio.PUD_UP)
+        for pin in [rotaryA, rotaryB, switchPin]:
+            self.pi1.set_mode(pin, pigpio.INPUT)
+            self.pi1.set_pull_up_down(pin, pigpio.PUD_UP)
 
-        self.pi1.set_mode(self.switchPin, pigpio.INPUT)
-        self.pi1.set_pull_up_down(self.switchPin, pigpio.PUD_UP)
+        self.pi1.set_glitch_filter(self.rotaryA, 3000)
+        self.pi1.set_glitch_filter(self.rotaryB, 3000)
+        self.pi1.set_glitch_filter(self.switchPin, 20000)
 
-        self.pi1.set_glitch_filter(switchPin, 50000)
-
-        #set up other vars
-        self.readA = None
-        self.readB = None
-        self.prevA = None
-        self.clockwise = None
+        self.changed = False
+        self.clockwise = False
         self.fast = False
-        self.resistance = 100
+        self.prevA = None
 
-    #check direction and speed of encoder spinning    
     async def checkRotary(self):
-        startTime = time.perf_counter()
         self.prevA = self.pi1.read(self.rotaryA)
+        startTime = time.perf_counter()
         
         while True:
-            self.readA = self.pi1.read(self.rotaryA) #find current A pin value
-
-            #if rotary encoder is spinning
-            if self.readA != self.prevA:
+            readA = self.pi1.read(self.rotaryA)
+            if readA != self.prevA and readA == 0:
                 endTime = time.perf_counter()
-                print("End Time:", endTime)
-                print("Click!")
-                #checks speed
-                if abs(startTime - endTime) >= 2:
-                    self.fast = False
-                    print("Slow")
-                else:
-                    self.fast = True
-                    print("Fast")
-                startTime = time.perf_counter()
-                print("Start Time:", startTime)
-                #checks direction
-                if self.pi1.read(self.rotaryB) != self.readA:
-                    self.clockwise = True
-                    print("Clockwise")
-                else:
-                    self.clockwise = False
-                    print("Counterclockwise")
-
-            #update A value
-            self.prevA = self.readA
-
-            #lets other coroutines run
-            await asyncio.sleep(0)
-
-# --- For Testing ---
-if __name__ == "__main__":
-
-    pi1 = pigpio.pi()
-    rot = Rotary(18, 23, 24, pi1)
-    asyncio.run(rot.checkRotary())
+                self.fast = (endTime - startTime) < 0.2
+                startTime = endTime
+                self.clockwise = (self.pi1.read(self.rotaryB) == 1)
+                self.changed = True
+            
+            self.prevA = readA
+            await asyncio.sleep(0.001)
