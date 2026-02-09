cf1b6e6e35cb784e9372dc1168d5f0db9b3e75e8
SenWasHere <152115255+SenWasHere@users.noreply.github.com>
2026-02-08 21:59:20 -0500

    Remove print statements in RotaryFunctions.py

--- Checkpoint_C/RotaryFunctions.py
+++ Checkpoint_C/RotaryFunctions.py
@@ -100,22 +100,22 @@ class Rotary:
             if self.pi1.wait_for_edge(self.switchPin, 1):
                 self.clicked = True
                 startTime = time.perf_counter()
-                print("Press")
+                #print("Press")
                 #print("Start Time Button:", startTime)
 
                 #if button is no longer pressed
                 if self.pi1.wait_for_edge(self.switchPin, pigpio.EITHER_EDGE):
                     endTime = time.perf_counter()
-                    print("Stop Press")
+                    #print("Stop Press")
                     #print("End Time Button:", endTime)
 
                     #checks duration
                     if abs(startTime - endTime) >= 3:
                         self.longClicked = True
-                        print("Long")
+                        #print("Long")
                     else:
                         self.longClicked = False
-                        print("Short")
+                        #print("Short")
 
                     #lets other coroutines run
                     #time.sleep(0.01)
