7af4b9d3955276f23ee2a3e14a7d90bd29a98a16
SenWasHere <152115255+SenWasHere@users.noreply.github.com>
2026-02-10 18:54:53 -0500

    Change menu switching in CheckpointC.py

--- Checkpoint_C/CheckpointC.py
+++ Checkpoint_C/CheckpointC.py
@@ -56,10 +56,9 @@ while True:
             lcd.lcd_display_string("   DigiPot1", 2)
             print("=> DigiPot0    Digipot1")
             #insert proper LCD updating code here
-        if rotary.rotating == True:
-            if rotary.clockwise == True:
-                state = "menu1"
-                menu1First = True
+        if rotary.rotating == True and rotary.clockwise == True:
+            state = "menu1"
+            menu1First = True
         elif rotary.clicked == True:
             state = "digi0"
             digi0First = True
@@ -73,10 +72,9 @@ while True:
                 lcd.lcd_display_string("   Digipot0", 1)
                 lcd.lcd_display_string("=> DigiPot1", 2)
                 print("   DigiPot0 => Digipot1")
-            if rotary.rotating == True:
-                if rotary.clockwise == False:
-                    state = "menu0"
-                    menu0First = True
+            if rotary.rotating == True and rotary.clockwise == False:
+                state = "menu0"
+                menu0First = True
             elif rotary.clicked == True:
                 state = "digi1"
                 digi1First = True
