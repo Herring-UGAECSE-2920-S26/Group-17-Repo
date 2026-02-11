c79a57411077e813e3d98974a338ff3f849677cc
thomasprybylski <top91741@uga.edu>
2026-02-10 22:00:44 -0500

    Refactor Rotary class
    Updated glitch filter timings and added a counter system for rotary encoder.

--- Checkpoint_C/RotaryFunctions.py
+++ Checkpoint_C/RotaryFunctions.py
@@ -4,134 +4,68 @@ import threading
 import time
 
 class Rotary:
-
-    #min and max resistor values
     minR = 100
     maxR = 10000
 
-    #initialize Rotary object
     def __init__(self, rotaryA, rotaryB, switchPin, pi1):
         self.rotaryA = rotaryA
         self.rotaryB = rotaryB
         self.switchPin = switchPin
         self.pi1 = pi1
 
-        #set up rotary encoder
+        # Setup pins
         self.pi1.set_mode(self.rotaryA, pigpio.INPUT)
         self.pi1.set_pull_up_down(self.rotaryA, pigpio.PUD_UP)
-        self.pi1.set_glitch_filter(self.rotaryA, 3000) # 3ms debounce
+        self.pi1.set_glitch_filter(self.rotaryA, 1000) 
         
         self.pi1.set_mode(self.rotaryB, pigpio.INPUT)
         self.pi1.set_pull_up_down(self.rotaryB, pigpio.PUD_UP)
-        self.pi1.set_glitch_filter(self.rotaryB, 3000) # 3ms debounce
+        self.pi1.set_glitch_filter(self.rotaryB, 1000) 
 
         self.pi1.set_mode(self.switchPin, pigpio.INPUT)
         self.pi1.set_pull_up_down(self.switchPin, pigpio.PUD_UP)
-        self.pi1.set_glitch_filter(self.switchPin, 20000) # 20ms debounce
+        self.pi1.set_glitch_filter(self.switchPin, 10000) 
 
-        #set up other vars
-        self.readA = None
-        self.readB = None
-        self.prevA = None
-        self.clockwise = None
-        self.fast = False
-        self.rotating = False
+        # NEW: Counter system
+        self.tally = 0
         self.clicked = False
-        self.longClick = False
+        self.longClicked = False
+        self.fast = False 
 
-    #check direction and speed of encoder spinning    
     def checkRotary(self):
-        startTime = time.perf_counter()
-        self.prevA = self.pi1.read(self.rotaryA)
+        last_clk = self.pi1.read(self.rotaryA)
+        last_time = time.time()
         
         while True:
-            self.readA = self.pi1.read(self.rotaryA) #find current A pin value
+            current_clk = self.pi1.read(self.rotaryA)
+            if current_clk != last_clk:
+                current_time = time.time()
+                # Speed Check
+                self.fast = (current_time - last_time) < 0.05
+                last_time = current_time
 
-            #if rotary encoder is spinning
-            if self.readA != self.prevA:
-                self.rotating = True
-                endTime = time.perf_counter()
-                #print("End Time Rot:", endTime)
-                #print("Click!")
-                
-                #checks speed
-                if abs(startTime - endTime) >= 1:
-                    self.fast = False
-                    #print("Slow")
+                # Direction Check
+                if current_clk != self.pi1.read(self.rotaryB):
+                    self.tally += 1 
                 else:
-                    self.fast = True
-                    #print("Fast")
-                startTime = time.perf_counter()
-                #print("Start Time Rot:", startTime)
-                
-                #checks direction
-                if self.pi1.read(self.rotaryB) != self.readA:
-                    self.clockwise = True
-                    #print("Clockwise")
-                    #print("Is Rotating:", self.rotating)
-                    #lets other coroutines run
-                    #time.sleep(0.01)
-                else:
-                    self.clockwise = False
-                    #print("Counterclockwise")
-                    #print("Is Rotating:", self.rotating)
-                    #lets other coroutines run
-                    #time.sleep(0.01)
-            else:
-                self.rotating = False
-                #print("Is Rotating:", self.rotating)
-                #lets other coroutines run
-                #time.sleep(0.01)
+                    self.tally -= 1
+                last_clk = current_clk
+            
+            # 1ms sleep: Fast enough for direction, slow enough for CPU
+            time.sleep(0.001)
 
-            #update A value
-            self.prevA = self.readA
-
-            #lets other coroutines run
-            time.sleep(0.0001)
-
-            #update rotating
-            #self.rotating = False
-
-    #checks if button is pressed and for how long
     def checkButton(self):
-
         while True:
-
-            #if button has been pressed
-            if self.pi1.wait_for_edge(self.switchPin, 1):
-                self.clicked = True
-                startTime = time.perf_counter()
-                #print("Press")
-                #print("Start Time Button:", startTime)
-
-                #if button is no longer pressed
-                if self.pi1.wait_for_edge(self.switchPin, pigpio.EITHER_EDGE):
-                    endTime = time.perf_counter()
-                    #print("Stop Press")
-                    #print("End Time Button:", endTime)
-
-                    #checks duration
-                    if abs(startTime - endTime) >= 3:
-                        self.longClicked = True
-                        #print("Long")
-                    else:
-                        self.longClicked = False
-                        #print("Short")
-
-                    #lets other coroutines run
-                    #time.sleep(0.01)
-
-                    #updates values
-                    #self.clicked = False
-                    #self.longClicked = False
-
-                   #lets other coroutines run
-                    time.sleep(0.0001)
-                    
-            #if button hasn't been pressed
-            self.clicked = False
-             #lets other coroutines run
-            #time.sleep(0.01)
+            # Wait for press
+            if self.pi1.read(self.switchPin) == 0:
+                start = time.time()
+                while self.pi1.read(self.switchPin) == 0:
+                    time.sleep(0.01)
+                
+                duration = time.time() - start
+                if duration > 2.0: self.longClicked = True
+                elif duration > 0.05: self.clicked = True
+            time.sleep(0.05)
 
 # --- For Testing ---
 if __name__ == "__main__":
