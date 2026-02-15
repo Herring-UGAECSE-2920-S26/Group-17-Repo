c121e48b7089c6c667d3fd085c81a326e3e51bcd
Sen <sen10732@uga.edu>
2026-02-15 00:18:31 -0500

    Wrote versions of the rotary and CheckpointC scripts that use callbacks

---
+++ Checkpoint_C/CheckpointC_Driver.py
@@ -0,0 +1,136 @@
+import spidev #https://pypi.org/project/spidev/
+import pigpio #https://abyz.me.uk/rpi/pigpio/index.html
+import time
+import Dual_Digipot
+import Rotary
+import I2C_LCD_driver #https://gist.github.com/DenisFromHR/cc863375a6e19dce359d
+import Min_difference
+
+#set up libraries
+pi1 = pigpio.pi()
+spi1 = spidev.SpiDev()
+
+#set up devices
+rotary = Rotary.Rotary(18,23,24, pi1)
+digipot = Dual_Digipot.MCP4131(spi1)
+lcd = I2C_LCD_driver.lcd()
+
+#declare vars
+state = "menu0"
+digi0R = 100
+digi1R = 100
+minR = 100
+maxR = 10000
+menu0First = True
+menu1First = True
+digi0First = True
+digi1First = True
+
+try:
+
+    while True:
+        clockwise, fast = rotary.getRotary()
+        clicked, longClick = rotary.getButton()
+
+        #main menu part 0
+        if state == "menu0":
+            #updates led when something has changed
+            if menu0First == True:
+                lcd.lcd_clear()
+                lcd.lcd_display_string("=> DigiPot0", 1)
+                lcd.lcd_display_string("   DigiPot1", 2)
+                print("=> DigiPot0    Digipot1")
+                menu0First = False
+                
+            #switch to other menu option if rotating
+            if clockwise != 0: 
+                state = "menu1"
+                menu1First = True
+            #switch to digipot 0 if clicked
+            elif clicked == True:
+                state = "digi0"
+                digi0First = True
+        
+        #main menu part 1
+        elif state == "menu1":
+            #updates led when something has changed
+            if menu1First == True:
+                lcd.lcd_clear()
+                lcd.lcd_display_string("   DigiPot0", 1)
+                lcd.lcd_display_string("=> DigiPot1", 2)
+                print("   DigiPot0 => Digipot1")
+                menuFirst = False
+                
+                #switch to other menu option if rotating
+                if clockwise != 0: 
+                    state = "menu0"
+                    menu0First = True 
+                #switch to digipot1 if clicked  
+                elif clicked == True:
+                    state = "digi1"
+                    digi1First = True
+
+        #DigiPot0 handling
+        elif state == "digi0":
+            
+            #updates led when something has changed
+            if digi0First == True:
+                lcd.lcd_clear()
+                lcd.lcd_display_string("DigiPot 0 Resistance", 1)
+                lcd.lcd_display_string(f"{digi0R} Ohms", 2)
+                print(digi0R)
+                digi0First = False
+
+                if clockwise != 0:
+                    stepSize = 100 if fast else 10
+                    digi0R += (clockwise * stepSize)
+
+                    if digi0R > maxR: digi0R = maxR
+                    if digi0R < minR: digi0R = minR
+
+                    lcd.lcd_display_string(f"{digi0R} Ohms", 2)
+
+                if longClicked:
+                    state = "menu0"
+                    menu0First = True
+                elif clicked:
+                    print("Updated DigiPot0")
+                    step = Min_difference.min_difference(float(digi0R/1000))
+                    digipot.set_step(step, 1)
+
+                    
+        #DigiPot1 handling
+        elif state == "digi1":
+            
+            #updates led when something has changed
+            if digi1First == True:
+                lcd.lcd_clear()
+                lcd.lcd_display_string("DigiPot 1 Resistance", 1)
+                lcd.lcd_display_string(f"{digi1R} Ohms", 2)
+                print(digi1R)
+                digi1First = False
+
+                if clockwise != 0:
+                    stepSize = 100 if fast else 10
+                    digi1R += (clockwise * stepSize)
+
+                    if digi1R > maxR: digi1R = maxR
+                    if digi1R < minR: digi1R = minR
+
+                    lcd.lcd_display_string(f"{digi1R} Ohms", 2)
+
+                if longClicked:
+                    state = "menu0"
+                    menu0First = True
+                elif clicked:
+                    print("Updated DigiPot1")
+                    step = Min_difference.min_difference(float(digi1R/1000))
+                    digipot.set_step(step, 1)
+
+
+        time.sleep(0.05)
+        
+
+except KeyboardInterrupt:
+    rotary.cancel()
+    lcd.lcd_clear()
---
+++ Checkpoint_C/Rotary.py
@@ -0,0 +1,96 @@
+import pigpio #https://abyz.me.uk/rpi/pigpio/index.html
+import time
+
+#class that sets up the rotary encoder component and checks status of 
+#rotary encoder and button
+class Rotary:
+
+    #initialize Rotary object
+    def __init__(self, rotaryA, rotaryB, switchPin, pi1):
+        self.rotaryA = rotaryA
+        self.rotaryB = rotaryB
+        self.switchPin = switchPin
+        self.pi1 = pi1
+
+        #set up rotary encoder
+        self.pi1.set_mode(self.rotaryA, pigpio.INPUT)
+        self.pi1.set_pull_up_down(self.rotaryA, pigpio.PUD_UP)
+        self.pi1.set_glitch_filter(self.rotaryA, 1000) # 3ms debounce
+        
+        self.pi1.set_mode(self.rotaryB, pigpio.INPUT)
+        self.pi1.set_pull_up_down(self.rotaryB, pigpio.PUD_UP)
+        self.pi1.set_glitch_filter(self.rotaryB, 1000) # 3ms debounce
+
+        self.pi1.set_mode(self.switchPin, pigpio.INPUT)
+        self.pi1.set_pull_up_down(self.switchPin, pigpio.PUD_UP)
+        self.pi1.set_glitch_filter(self.switchPin, 20000) # 20ms debounce
+
+        #set up other vars
+        self.clockwise = None
+        self.fast = False
+        self.clicked = False
+        self.longClick = False
+        self.lastTick = 0
+
+        #create callbacks
+        self.rotaryCallback = self.pi1.callback(self.rotaryA, pi1.EITHER_EDGE, self.rotaryFunction)
+        self.buttonCallback = self.pi1.callback(self.switchPin, pi1.EITHER_EDGE, self.buttonFunction)
+        
+
+    def rotaryFunction(self, gpio, level, tick):
+
+        if level == 1:
+            
+            self.fast = (50000 > self.pi1.tickDiff(self.lastTick, tick))
+            self.lastTick = tick
+
+            if self.pi1.read(self.rotaryB) == 0:
+                self.clockwise = 1 #clockwise
+            else:
+                self.clockwise = -1 #counterclockwise
+
+                
+    def buttonFunction(self, gpio, level, tick):
+
+        if level == 0:
+            self.clicked = True
+            looped = True
+            startTime = tick
+
+            while (looped):
+                endTime = self.pi1.get_current_tick()
+                if (self.pi1.tickDiff(startTime, endTime) > 3000000):
+                    self.longClick = True
+                    self.looped = False
+                if level == 1:
+                    looped = False
+
+
+    def getRotary(self):
+        
+        readClockwise = self.clockwise
+        readFast = self.fast
+
+        self.clockwise = 0
+
+        return readClockwise, readFast
+
+
+    def getButton(self):
+
+        readClicked = self.clicked
+        readLongClick = self.longClick
+
+        self.clicked = False
+        self.longClick = False
+
+        return readClicked, readLongClick
+
+
+    def cancel(self):
+        self.rotaryCallback.cancel()
+        self.buttonCallback.cancel()
+        
+                
+            
+            
