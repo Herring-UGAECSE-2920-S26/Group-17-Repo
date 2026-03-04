4053ae3aa6ae0e2781661eb37a8863e1efc98cbf
Cecelia Kane <cjk23503@uga.edu>
2026-03-04 00:32:56 -0500

    Update empirical calibration curve for voltage calculation

--- Deliverable09/ADCvoltmeter.py
+++ Deliverable09/ADCvoltmeter.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
     if t2 is not None:
             # --- Updated Empirical Calibration Curve ---
             # Derived from your latest data: Vin = (slope * t2) + intercept
-            vin = (0.00008333 * t2) - 5.0113
+            vin = ( 0.00028075* t2) - 5.1162
     
             print(f"Actual T1: {t1:.0f} us | Actual T2: {t2:.0f} us")
             print(f"Measured Vin: {vin:.4f} V")
