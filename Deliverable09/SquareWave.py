53b56e1f786420bba782d6f6c7b6131ea4dadbc3
SenWasHere <152115255+SenWasHere@users.noreply.github.com>
2026-03-17 22:42:45 -0400

    Update SquareWave.py

--- Deliverable09/SquareWave.py
+++ Deliverable09/SquareWave.py
@@ -15,7 +15,7 @@ def waveOff(gpio, pi):
 
 #function that updates the amplitude of the square wave
 def updateAmplitude(amplitude, digipot):
-    step = Min_difference.min_difference_volts(amplitude/2)
+    step = Min_difference.min_difference_volts(amplitude)
     digipot.set_step(step, 1)
 
 # --- For Testing ---
