3b2b45368c0e0e835f513888296ed38e39e02abb
Sen <sen10732@uga.edu>
2026-03-18 00:52:18 -0400

    Added supporting files to Deliverable08

---
+++ Deliverable08/Dual_Digipot.py
@@ -0,0 +1,55 @@
+import spidev #https://pypi.org/project/spidev/
+import sys
+
+
+class MCP4131:
+    def __init__(self, spi, bus=0, device=0):
+        self.spi = spi
+        self.spi.open(bus, device)
+        self.spi.max_speed_hz = 1000000 
+
+    #digipot values
+    def set_step(self, step, pot_num=0):
+        """Set the wiper position (0 to 128). pot_num: 0 or 1"""
+        if 0 <= step <= 128:
+            cmd = 0x00 if pot_num == 0 else 0x10
+            self.spi.xfer2([cmd, step])
+            print(f"set Pot {pot_num} wiper to step {step}")
+        else:
+            print("Step must be between 0 and 128.")
+
+    def close(self):
+        self.spi.close()
+
+#control for user input 
+if __name__ == "__main__":
+    spi = spidev.SpiDev()
+    pot = MCP4131(spi)
+    
+    print("manual digipot control")
+    print("Type 'exit' to quit.")
+
+    #code that allows the user to change the resistor value
+    #through keyboard input
+    try:
+        while True:
+            pot_input = input("\nEnter Pot Number (0 or 1): ").strip().lower()
+            if pot_input == 'exit':
+                break
+            
+            step_input = input("Enter Step Value (0-128): ").strip().lower()
+            if step_input == 'exit':
+                break
+
+            try:
+                p = int(pot_input)
+                s = int(step_input)
+                pot.set_step(s, pot_num=p)
+            except ValueError:
+                print("Invalid input. Please enter whole numbers.")
+
+    except KeyboardInterrupt:
+        print("\nProgram interrupted.")
+    finally:
+        pot.close()
+        print("SPI connection closed.")
---
+++ Deliverable08/Rotary.py
@@ -0,0 +1,147 @@
+import pigpio #https://abyz.me.uk/rpi/pigpio/index.html
+import time
+
+#class that sets up the rotary encoder component and checks status of 
+#rotary encoder and button
+class Rotary:
+
+    #initialize Rotary object
+    def __init__(self, rotaryA, rotaryB, switchPin, pi1):
+        self.rotaryA = rotaryA
+        self.rotaryB = rotaryB
+        self.switchPin = switchPin
+        self.pi1 = pi1
+
+        #set up rotary encoder
+        self.pi1.set_mode(self.rotaryA, pigpio.INPUT)
+        self.pi1.set_pull_up_down(self.rotaryA, pigpio.PUD_UP)
+        self.pi1.set_glitch_filter(self.rotaryA, 1000) # 3ms debounce
+        
+        self.pi1.set_mode(self.rotaryB, pigpio.INPUT)
+        self.pi1.set_pull_up_down(self.rotaryB, pigpio.PUD_UP)
+        self.pi1.set_glitch_filter(self.rotaryB, 1000) # 3ms debounce
+
+        self.pi1.set_mode(self.switchPin, pigpio.INPUT)
+        self.pi1.set_pull_up_down(self.switchPin, pigpio.PUD_UP)
+        self.pi1.set_glitch_filter(self.switchPin, 20000) # 20ms debounce
+
+        #set up other vars
+        self.clockwise = 0
+        self.fast = False
+        self.clicked = False
+        self.longClick = False
+        self.lastTick = 0
+
+        #create callbacks
+        self.rotaryCallback = self.pi1.callback(self.rotaryA, pigpio.EITHER_EDGE, self.rotaryFunction)
+        self.buttonCallback = self.pi1.callback(self.switchPin, pigpio.EITHER_EDGE, self.buttonFunction)
+
+    
+    #callback function that checks speen and direction of encoder spinning
+    def rotaryFunction(self, gpio, level, tick):
+
+        if level == 1: #if A value is 1
+            levelB = self.pi1.read(self.rotaryB) #gets B value
+            #print("Rotation Detected")
+
+            #checks if fast
+            self.fast = (500000 > pigpio.tickDiff(self.lastTick, tick))
+            self.lastTick = tick
+            #print("Fast:", self.fast)
+
+            #finds direction
+            if levelB == 0:
+                self.clockwise = 1 #clockwise
+            else:
+                self.clockwise = -1 #counterclockwise
+
+    #callback function that checks if button is pressed and if long press
+    def buttonFunction(self, gpio, level, tick):
+
+        #if pressed
+        if level == 0:
+            #print("Click Detected")
+            self.clicked = True
+            looped = True
+            startTime = tick
+
+            looped = self.pi1.read(self.switchPin)
+
+            #exits if long press and sets longClick to True
+            while looped != 1:
+                endTime = self.pi1.get_current_tick()
+                if (pigpio.tickDiff(startTime, endTime) > 3000000):
+                    self.longClick = True
+                    looped = 1
+                else: looped = self.pi1.read(self.switchPin)
+                
+                
+    #returns and resets clockwise and fast
+    def getRotary(self):
+        
+        readClockwise = self.clockwise
+        readFast = self.fast
+
+        self.clockwise = 0
+
+        return readClockwise, readFast
+
+    
+    #returns and resets clicked and longClick
+    def getButton(self):
+
+        readClicked = self.clicked
+        readLongClick = self.longClick
+
+        self.clicked = False
+        self.longClick = False
+
+        return readClicked, readLongClick
+
+
+    #closes the callbacks
+    def cancel(self):
+        self.rotaryCallback.cancel()
+        self.buttonCallback.cancel()
+
+# --- For Testing ---
+if __name__ == "__main__":
+
+    #setup
+    pi1 = pigpio.pi()
+    rotary = Rotary(18, 23, 24, pi1)
+
+    #update values
+    clockwise, fast = rotary.getRotary()
+    clicked, longClick = rotary.getButton()
+
+    #initial prints
+    print("Main Clockwise:", clockwise)
+    print("Main Fast:", fast)
+    print("Main Clicked:", clicked)
+    print("Main LongClick:", longClick)
+
+    #set previous values
+    prevClockwise = clockwise
+    prevFast = fast 
+    prevClicked = clicked
+    prevLongClick = longClick
+    
+    while True:
+        #update values
+        clockwise, fast = rotary.getRotary()
+        clicked, longClick = rotary.getButton()
+
+        #print new value if changed
+        if prevClockwise != clockwise: print("Main Clockwise:", clockwise)
+        if prevFast != fast: print("Main Fast:", fast)
+        if prevClicked != clicked: print("Main Clicked:", clicked)
+        if prevLongClick != longClick: print("Main LongClick:", longClick)
+
+        #set new previous values
+        prevClockwise = clockwise
+        prevFast = fast 
+        prevClicked = clicked
+        prevLongClick = longClick
+        
+
