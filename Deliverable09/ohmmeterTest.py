1102e8db39bc76016ea195e3b18641f2aac4da50
thomasprybylski <top91741@uga.edu>
2026-03-25 05:46:49 -0400

    Update Ohms calculation formula in ohmmeterTest.py

--- Deliverable09/ohmmeterTest.py
+++ Deliverable09/ohmmeterTest.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
         #vin = (0.000122* t2) - 6.96
         
         # 2. Calculate Ohms from Vin (Using ** for power)
-        ohms = (0.0000123*(t2*t2)) - 0.505*t2 + 3704
+        ohms = -8093 + 1.85 * t2 - 4.37E-05 * t2**2
     
         # --- Terminal Output ---
         print("-" * 30)
