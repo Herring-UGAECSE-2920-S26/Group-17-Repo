aa5a73f3bc63d1ccaae70efc73b40b3f0b83b443
SenWasHere <152115255+SenWasHere@users.noreply.github.com>
2026-04-06 10:23:20 -0400

    Update SineWave.py

--- Checkpoint_A/SineWave.py
+++ Checkpoint_A/SineWave.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
         #generates 500Hz 10V sine wave
         while True:
             currentTime = time.time()
-            sineWave.runner(currentTime, 1000, 10)
+            sineWave.runner(currentTime, 10000, 10)
 
     except KeyboardInterrupt:
         print("\nProgram interrupted.")
