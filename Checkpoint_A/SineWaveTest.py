81ab1d68c8d03ef422b6dd14ba02298d6d5419a4
thomasprybylski <top91741@uga.edu>
2026-04-06 15:29:40 -0400

    Change sine wave frequency from 10kHz to 5kHz

--- Checkpoint_A/SineWaveTest.py
+++ Checkpoint_A/SineWaveTest.py
@@ -90,7 +90,7 @@ if __name__ == "__main__":
         
         # You only need to call this ONCE. 
         # The DMA hardware takes over and loops it forever.
-        sineWave.start_wave(freq=10000)
+        sineWave.start_wave(freq=5000)
         
         # Look at your CPU usage now! Your main loop doesn't have to do any math.
         while True:
