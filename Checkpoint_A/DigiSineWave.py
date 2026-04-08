5439baf940405146cba22f8bbc28634d07e34046
thomasprybylski <top91741@uga.edu>
2026-04-07 23:39:57 -0400

    Change test frequency from 1000Hz to 10000Hz

--- Checkpoint_A/DigiSineWave.py
+++ Checkpoint_A/DigiSineWave.py
@@ -103,7 +103,7 @@ if __name__ == "__main__":
     sineWave = SineWave(pi1)
 
     try: 
-        test_freq = 1000
+        test_freq = 10000
         print(f"Generating {test_freq}Hz sine wave at full hardware resolution...")
         
         # Start the wave at 100% volume
