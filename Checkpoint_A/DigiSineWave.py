9d4b3c6e955c5c09f3066f65b07a62b29269053d
thomasprybylski <top91741@uga.edu>
2026-04-10 13:49:46 -0400

    Update DigiSineWave.py

--- Checkpoint_A/DigiSineWave.py
+++ Checkpoint_A/DigiSineWave.py
@@ -214,7 +214,7 @@ if __name__ == "__main__":
 
     try: 
 
-        test_freq = 1000
+        test_freq = 10000
 
         print(f"Generating {test_freq}Hz sine wave at full hardware resolution...")
 
