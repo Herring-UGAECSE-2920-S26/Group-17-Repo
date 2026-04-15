74cc1b6fa63698abaa9f1d5153eed63a8c7e2b2f
SenWasHere <152115255+SenWasHere@users.noreply.github.com>
2026-04-15 09:33:47 -0400

    Update adcCodeTest.py

--- Checkpoint_A/adcCodeTest.py
+++ Checkpoint_A/adcCodeTest.py
@@ -118,6 +118,7 @@ class Voltmeter:
         if t1 is not None:
             # 2. Calculate Ohms from Vin (Using ** for power)
             rin = -1576 + -0.264*t2 + 1.67E-5 *t2*t2
+            if rin < 0: rin = 0
             print(f"Measured Rin: {rin:.4f} Ohms")
         else: 
             rin = 0
