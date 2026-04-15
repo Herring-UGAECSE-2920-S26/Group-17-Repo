bd8c4b114caabf5f731d0b9a03e0fc9b1c8b313f
SenWasHere <152115255+SenWasHere@users.noreply.github.com>
2026-04-15 10:57:41 -0400

    Update SquareWave.py

--- Checkpoint_A/SquareWave.py
+++ Checkpoint_A/SquareWave.py
@@ -15,6 +15,7 @@ def waveOff(gpio, pi):
 
 #function that updates the voltage of the square wave
 def updateVoltage(voltage, digipot):
+    print(f"Wave voltage: {voltage}")
     digipot.set_step(57, 0)
     step = Min_difference.min_difference_volts(voltage)
     digipot.set_step(step, 1)
