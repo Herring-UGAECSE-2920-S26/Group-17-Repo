7e1bc4130b8a2cea5b7cebf43f65f236ba89ca52
Cecelia Kane <cjk23503@uga.edu>
2026-04-21 12:37:06 -0400

    remove comment

--- Checkpoint_A/adcNew.py
+++ Checkpoint_A/adcNew.py
@@ -96,7 +96,7 @@ class Voltmeter:
                 print(f"Measured Vin: {vin:.4f} V")
             else: #positive voltage
                 # Vin ( 0.000226* t2) - 5.56
-                # Ensure the sign of Vref matches your integrator's direction
+                
                 vin = -6.45 +3.61E-4 *t2 + -1.58E-9 *t2*t2
                 print(f"Actual T1: {t1} us | Actual T2: {t2} us")
                 print(f"Measured Vin: {vin:.4f} V")
